8c6e6e12b0592eb2e448b88f3978c470fe02b32c
Muhammad Anas Raza <memanasraza@gmail.com>
2020-09-08 00:49:08 +0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+
+    return (
+      <React.Fragment>
+     <h2>
+       My name is {name}!
+     </h2>
+    </React.Fragment>
+    );
+}
+
+export default App;
